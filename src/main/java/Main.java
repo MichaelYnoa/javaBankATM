ac275a86f9f7aded74e6c54113ee62ba38ec9b52
MichaelAraujoMac <michaelaraujoynoa@gmail.com>
2025-03-27 17:38:25 -0400

    before changing local env

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,23 @@
-public class Main {
+public class Main  {
+    private String pin;
+
     public static void main(String[] args){
         System.out.println("Hello Java Bank ATM");
     }
+
+
+    public boolean authenticateUser(String inputPin) {
+        int attempts = 0;
+        while (attempts < 3) {
+            if (this.pin.equals(inputPin)) {
+                return true;
+            } else {
+                attempts++;
+                System.out.println("PIN incorrecto. Intento " + attempts + " de 3.");
+            }
+        }
+        return false;
+    }
+
 }
+
